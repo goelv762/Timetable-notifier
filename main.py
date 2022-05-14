d959447c86f10e01af8b792bb6410e94b851fbff
Varun Goel <89370605+Toa-st@users.noreply.github.com>
2022-05-14 14:53:34 +1200

    made function that keeps only the useful data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def parse_lines():
     TIMETABLE = open("./data/timetable.ics", "r")
-    ALLOWED_PREFIX = ["DTSTART", "DTEND", "SUMMARY", "LOCATION"]
+    ALLOWED_PREFIX = ["DTSTART;", "DTEND;", "SUMMARY", "LOCATION", "DESCRIPTION"]
 
     raw_timetable_lines = TIMETABLE.readlines()
     new_timetable_lines = []
@@ -9,5 +9,16 @@ def parse_lines():
         if (raw_timetable_lines[x].startswith(tuple(ALLOWED_PREFIX))):
             new_timetable_lines.append(raw_timetable_lines[x])
 
+    for y in range(len(new_timetable_lines)):
+        for z in range(len(ALLOWED_PREFIX)):
+            new_timetable_lines[y] = new_timetable_lines[y].replace(ALLOWED_PREFIX[z], "")
+        
+        colon_index = new_timetable_lines[y].find(':')
+        if (colon_index != -1):
+            new_timetable_lines[y] = new_timetable_lines[y][new_timetable_lines[y].find(':') + 1:]
+
     
+    temp = open("./data/temp.txt", "w")
+    temp.writelines(new_timetable_lines)
+  
 parse_lines()
